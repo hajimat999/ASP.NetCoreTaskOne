73e1a2ef944dc041312c563b7987a8f1875104b4
hajimat999 <101628136+hajimat999@users.noreply.github.com>
2022-07-11 03:09:30 +0400

    First

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers 
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Testimonial model = new Testimonial
+            {
+                Id = 1,
+                Title = "Testimonials",
+                Desc = "Necessitatibus eius consequatur ex aliquid fuga eum quidem",
+                Image1 = "testimonials-1.jpg",
+                Image2 = "testimonials-2.jpg",
+                Image3 = "testimonials-3.jpg",
+                Image4 = "testimonials-4.jpg",
+                Image5 = "testimonials-5.jpg",
+                Name1 = "Saul Goodman",
+                Name2 = "Sara Wilsson",
+                Name3 = "Jena Karlis",
+                Name4 = "Matt Brandon",
+                Name5 = "John Larson"
+            };
+            return View(model);
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/Testimonial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Testimonial
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public string Image1 { get; set; }
+        public string Name1 { get; set; }
+        public string Image2 { get; set; }
+        public string Name2 { get; set; }
+        public string Image3 { get; set; }
+        public string Name3 { get; set; }
+        public string Image4 { get; set; }
+        public string Name4 { get; set; }
+        public string Image5 { get; set; }
+        public string Name5 { get; set; }
+    }
+}
